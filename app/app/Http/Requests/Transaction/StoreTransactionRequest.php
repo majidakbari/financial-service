70595aa38d0668915dbe28c64abaab139b14c20a
Majid Akbari <majid.akbari@neocard.fi>
2020-09-13 17:51:04 +0300

    add validation rules for store transaction API

--- app/app/Http/Requests/Transaction/StoreTransactionRequest.php
+++ app/app/Http/Requests/Transaction/StoreTransactionRequest.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Requests\Transaction;
 
+use Illuminate\Validation\Rule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class StoreTransactionRequest extends FormRequest
 {
     public function rules(): array
     {
-        return [];
+        return [
+            'source_wallet_id' => ['required', 'int', Rule::exists('wallets', 'id')->where('is_active', true)],
+            'destination_wallet_id' => ['required', 'int', Rule::exists('wallets', 'id')->where('is_active', true)],
+            'amount' => ['required', 'numeric', 'min:0.1', 'regex:/^\d+(\.\d{1,2})?$/'],
+            'description' => ['nullable', 'string', 'max:65535']
+        ];
     }
 }
