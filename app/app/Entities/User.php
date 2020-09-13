b942f0fb687b205ff9c8e4edf0771d94d081afcd
Majid Akbari <majid.akbari@neocard.fi>
2020-09-13 12:35:51 +0300

    create migration for users table

--- app/app/Entities/User.php
+++ app/app/Entities/User.php
@@ -2,8 +2,18 @@
 
 namespace App\Entities;
 
+use Carbon\Carbon;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+/**
+ * Class User
+ * @property int id
+ * @property string name
+ * @property string email
+ * @property Carbon created_at
+ * @property Carbon updated_at
+ * @package App\Entities
+ */
 class User extends Authenticatable
 {
 }
